dab4d634c6858e59d54b2db063f6a82e4d41de2f
campen <campen@nyu.edu>
2020-04-30 13:50:53 +0200

    .

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/campen/Documents/Papers/BezierGuarding/code_git/BezierGuarding/src
-# Build directory: /Users/campen/Documents/Papers/BezierGuarding/code_git/BezierGuarding/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
